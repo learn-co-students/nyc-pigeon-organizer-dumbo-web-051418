880d9eb342b8723d6ff494a0da867c755240b9f5
Anthony Lam <al3101@nyu.edu>
2018-05-10 01:54:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |trait, value|
+    value.each do |key, birds|
+      birds.each do |bird|
+        pigeon_list[bird] = {}
+      end
+    end
+  end
+  
+  pigeon_list.collect do |bird, attributes|
+    data.each do |trait, value|
+      pigeon_list[bird][trait] = []
+    end
+  end
+  
+  pigeon_list.collect do |bird, attributes|
+    attributes.collect do |attribute, list|  
+      data.collect do |trait, value|
+        if trait == attribute
+          value.collect do |key, birds|
+            if birds.include?(bird)
+              pigeon_list[bird][trait] << key.to_s
+            end
+          end
+        end
+      end
+    end
+  end
+  pigeon_list
 end
