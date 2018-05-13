a99fd808906960c1abb7c2253df7dfa40edac625
Roniece Ricardo <roniecericardo@gmail.com>
2018-05-13 21:55:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_info = Hash.new
+  data.each do |symbol, hash|
+    hash.each do |val, arr|
+      arr.each do |pidge_name|
+      if !pigeon_info.include?(pidge_name)
+          pigeon_info[pidge_name] = {symbol => [val.to_s]}
+      elsif pigeon_info[pidge_name].include?(symbol)
+          pigeon_info[pidge_name][symbol] << val.to_s
+      else
+          pigeon_info[pidge_name][symbol] = [val.to_s]
+        end
+      end
+    end
+  end
+  pigeon_info
 end
